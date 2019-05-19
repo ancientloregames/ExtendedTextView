45d9a0cfd4b726a5491be7c6013082b8eeb94cdb
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-05-19 16:18:18 +0300

    added compat methods to set compound drawables tint

--- library/src/main/java/com/ancientlore/lib/extendedtextview/ExtendedTextView.java
+++ library/src/main/java/com/ancientlore/lib/extendedtextview/ExtendedTextView.java
@@ -1,7 +1,11 @@
 package com.ancientlore.lib.extendedtextview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
 import android.util.AttributeSet;
@@ -9,19 +13,41 @@ import android.util.AttributeSet;
 @SuppressWarnings("unused")
 public class ExtendedTextView extends AppCompatTextView
 {
+	@ColorInt
+	private int drawableTint = Color.TRANSPARENT;
+
 	public ExtendedTextView(Context context)
 	{
 		super(context);
+		init(context, null);
 	}
 
 	public ExtendedTextView(Context context, AttributeSet attrs)
 	{
 		super(context, attrs);
+		init(context, attrs);
 	}
 
 	public ExtendedTextView(Context context, AttributeSet attrs, int defStyleAttr)
 	{
 		super(context, attrs, defStyleAttr);
+		init(context, attrs);
+	}
+
+	private void init(@NonNull Context context, @Nullable AttributeSet attrs)
+	{
+		if (attrs != null)
+		{
+			TypedArray styles = context.obtainStyledAttributes(attrs, R.styleable.ExtendedTextView);
+
+			try {
+				drawableTint = styles.getColor(R.styleable.ExtendedTextView_drawableTint, Color.TRANSPARENT);
+			} finally {
+				styles.recycle();
+			}
+		}
+
+		updateDrawablesTint();
 	}
 
 	@Override
@@ -43,28 +69,33 @@ public class ExtendedTextView extends AppCompatTextView
 	public void setImages(@Nullable Drawable left, @Nullable Drawable top, @Nullable Drawable right, @Nullable Drawable bottom)
 	{
 		super.setCompoundDrawables(left, top, right, bottom);
+		updateDrawablesTint();
 	}
 
 	public void setLeftImage(@Nullable Drawable drawable)
 	{
+		Utils.setTint(drawable, drawableTint);
 		Drawable[] drawables = getCompoundDrawables();
 		super.setCompoundDrawables(drawable, drawables[1], drawables[2], drawables[3]);
 	}
 
 	public void setTopImage(@Nullable Drawable drawable)
 	{
+		Utils.setTint(drawable, drawableTint);
 		Drawable[] drawables = getCompoundDrawables();
 		super.setCompoundDrawables(drawables[0], drawable, drawables[2], drawables[3]);
 	}
 
 	public void setRightImage(@Nullable Drawable drawable)
 	{
+		Utils.setTint(drawable, drawableTint);
 		Drawable[] drawables = getCompoundDrawables();
 		super.setCompoundDrawables(drawables[0], drawables[1], drawable, drawables[3]);
 	}
 
 	public void setBottomImage(@Nullable Drawable drawable)
 	{
+		Utils.setTint(drawable, drawableTint);
 		Drawable[] drawables = getCompoundDrawables();
 		super.setCompoundDrawables(drawables[0], drawables[1], drawables[2], drawable);
 	}
@@ -72,5 +103,20 @@ public class ExtendedTextView extends AppCompatTextView
 	public void resetImages()
 	{
 		setCompoundDrawables(null, null, null, null);
+	}
+
+	public void setDrawableTint(@ColorInt int color)
+	{
+		drawableTint = color;
+		updateDrawablesTint();
+	}
+
+	private void updateDrawablesTint()
+	{
+		Drawable[] drawables = getCompoundDrawables();
+		Utils.setTint(drawables[0], drawableTint);
+		Utils.setTint(drawables[1], drawableTint);
+		Utils.setTint(drawables[2], drawableTint);
+		Utils.setTint(drawables[3], drawableTint);
 	}
 }
--- library/src/main/java/com/ancientlore/lib/extendedtextview/Utils.java
+++ library/src/main/java/com/ancientlore/lib/extendedtextview/Utils.java
@@ -1,6 +1,9 @@
 package com.ancientlore.lib.extendedtextview;
 
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.v4.graphics.drawable.DrawableCompat;
 
 public class Utils
 {
@@ -12,5 +15,13 @@ public class Utils
 			return;
 
 		drawable.setBounds(0, 0,  drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+	}
+
+	public static void setTint(Drawable drawable, @ColorInt int color)
+	{
+		if (drawable == null || color == Color.TRANSPARENT)
+			return;
+
+		DrawableCompat.setTint(drawable, color);
 	}
 }
