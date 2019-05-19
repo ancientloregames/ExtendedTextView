d16790ad4fcac8eb4619a231a332c543a6ec6059
Nickolay Makhyboroda <ancientloregames@gmail.com>
2019-05-19 15:57:18 +0300

    basic class for setting compound drawables separately

---
+++ library/src/main/java/com/ancientlore/lib/extendedtextview/ExtendedTextView.java
@@ -0,0 +1,66 @@
+package com.ancientlore.lib.extendedtextview;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+@SuppressWarnings("unused")
+public class ExtendedTextView extends AppCompatTextView
+{
+	public ExtendedTextView(Context context)
+	{
+		super(context);
+	}
+
+	public ExtendedTextView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+
+	public ExtendedTextView(Context context, AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+	}
+
+	@Override
+	public void setCompoundDrawables(@Nullable Drawable left, @Nullable Drawable top, @Nullable Drawable right, @Nullable Drawable bottom)
+	{
+		setImages(left, top, right, bottom);
+	}
+
+	public void setImages(@Nullable Drawable left, @Nullable Drawable top, @Nullable Drawable right, @Nullable Drawable bottom)
+	{
+		super.setCompoundDrawables(left, top, right, bottom);
+	}
+
+	public void setLeftImage(@Nullable Drawable drawable)
+	{
+		Drawable[] drawables = getCompoundDrawables();
+		super.setCompoundDrawables(drawable, drawables[1], drawables[2], drawables[3]);
+	}
+
+	public void setTopImage(@Nullable Drawable drawable)
+	{
+		Drawable[] drawables = getCompoundDrawables();
+		super.setCompoundDrawables(drawables[0], drawable, drawables[2], drawables[3]);
+	}
+
+	public void setRightImage(@Nullable Drawable drawable)
+	{
+		Drawable[] drawables = getCompoundDrawables();
+		super.setCompoundDrawables(drawables[0], drawables[1], drawable, drawables[3]);
+	}
+
+	public void setBottomImage(@Nullable Drawable drawable)
+	{
+		Drawable[] drawables = getCompoundDrawables();
+		super.setCompoundDrawables(drawables[0], drawables[1], drawables[2], drawable);
+	}
+
+	public void resetImages()
+	{
+		setCompoundDrawables(null, null, null, null);
+	}
+}
